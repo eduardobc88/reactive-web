acaa04463584c3892d45e4a332d7db37d4968043
eduardo <eduardobc.88@gmail.com>
2018-09-03 16:57:10 -0500

    storage manifest added to settings

--- reactive-solutions/website/website/settings.py
+++ reactive-solutions/website/website/settings.py
@@ -128,3 +128,6 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
